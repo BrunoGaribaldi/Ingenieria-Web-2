25cb16c5a2528909c129f3f5d967ac37219314fe
joaquincernik <joaquincernik@gmail.com>
2025-05-12 16:08:13 -0300

    esqueleto del pryoecto

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//para las rutas de usuario
+
+
+//aca vamos a mostrar la vista home
 app.get('/', (req, res) => {
   res.send('¡Hola mundo desde Express!');
 });
+
+
+
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
