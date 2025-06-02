8598ef02a58168150452a6258a1cefa13591a038
Bruno <115671955+BrunoGaribaldi@users.noreply.github.com>
2025-06-02 14:18:34 -0300

    Add files via upload
    agregue esto como para en el caso de que querramos probar la app ya con algunos datos cargados

---
+++ Registros para BD de Prueba/Proyecto-ingenieria-web-2_productos.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: Proyecto-ingenieria-web-2
+-- ------------------------------------------------------
+-- Server version	8.0.42-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `productos`
+--
+
+LOCK TABLES `productos` WRITE;
+/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
+INSERT INTO `productos` VALUES (16,'Hat','uhhiuhu','gorra alce','unisex','foto-1748353907083-749424492.jpeg','20000','accesorio'),(17,'Hat','','gorra tero','unisex','foto-1748354078526-629860953.jpeg','20000','accesorio'),(18,'Hat','','Gorra gatito','unisex','foto-1748354150627-461355854.jpeg','20000','accesorio'),(19,'Oassian','Disponibles desde el talle S-XL','Campera corta ecocuero','mujer','foto-1748354527046-792221266.jpeg','30000','abrigo'),(21,'This week','','sweater britanico','hombre','foto-1748355007398-511895342.jpeg','59500','abrigo'),(22,'This week','Talles S-XL, colores azul y beige','sweater algodon ROB','hombre','foto-1748355124119-97737797.jpeg','59500','abrigo'),(23,'This week','Cremolatti','Buzo granizado con oreo','hombre','foto-1748355341958-390195194.jpeg','67500','abrigo'),(24,'Oassian','Colores rojo,amaillo, blanco disponibles','Buzo corderito LILY','mujer','foto-1748355250244-264944790.jpeg','35000','abrigo'),(25,'Diosa Luna','','Jean MOM negro estatico','mujer','foto-1748355446291-595516767.jpeg','45000','pantalon'),(26,'This week','','Sweater DEAL','hombre','foto-1748355491388-16449094.jpeg','59500','abrigo'),(27,'Oassian','','Buzo rustico MORZINE','mujer','foto-1748355720090-629141478.jpeg','44000','abrigo'),(28,'Zohue','Promocion ','jeans sueltos','mujer','foto-1748355897620-903409274.jpeg','25000','pantalon'),(29,'Zohue','','Tapado DIONISIA','mujer','foto-1748355992009-65359037.jpeg','79000','abrigo'),(30,'Zohue','','Campera RYAN','mujer','foto-1748356057588-753864314.jpeg','83000','abrigo'),(31,'Oassian','','Campera jean DENALI','mujer','foto-1748356103054-856938844.jpeg','73000','abrigo'),(32,'This week','','Sweater MILANO','hombre','foto-1748356140405-893491864.jpeg','62000','abrigo'),(33,'This week','','Campera red','hombre','foto-1748356202490-991078348.jpeg','56000','abrigo'),(34,'Zohue','','Blazer ','mujer','foto-1748356234773-88816054.jpeg','65000','abrigo'),(35,'Mistral','','Sweaters mistral colores','hombre','foto-1748356531191-306528288.jpeg','50000','abrigo'),(36,'Mistral','','Sweater algodon Rojo','hombre','foto-1748356570911-722878442.jpeg','56000','abrigo'),(37,'Mistral','','Chomba rayada ','hombre','foto-1748356609976-865033646.jpeg','45000','remera'),(38,'Mistral','','Chomba one stripe','hombre','foto-1748356643079-155356439.jpeg','45000','remera'),(39,'Mistral','','Remeras plain','hombre','foto-1748356669156-213569736.jpeg','30000','remera'),(40,'Red Cross','','Remera WAY OUT','hombre','foto-1748356700558-601165835.jpeg','23000','remera'),(41,'inside','','Remera SAY IT LOUD','hombre','foto-1748356741675-167975964.jpeg','23000','remera'),(42,'This week','','Campera red striped','hombre','foto-1748356786960-764944129.jpeg','50000','abrigo'),(43,'Oassian','','Buzo verde','mujer','foto-1748356818108-534660503.jpeg','43000','abrigo'),(44,'This week','','Campera verde','hombre','foto-1748356851952-94657460.jpeg','65000','abrigo'),(45,'Red Cross','','Remera NEY YORK','hombre','foto-1748356886721-493317238.jpeg','20000','remera'),(46,'Inedita','','Remera lisas','mujer','foto-1748356922523-132977518.jpeg','13000','remera'),(47,'This week','','Cintos','hombre','foto-1748356957218-803838440.jpeg','23000','accesorio'),(48,'This week','','Remera FLAG','hombre','foto-1748356991958-97997184.jpeg','18000','remera'),(49,'This week','','Remera white','hombre','foto-1748357022001-895416402.jpeg','17000','remera'),(50,'Red Cross','','Remera SEVEN UP','hombre','foto-1748357055303-523293314.jpeg','23000','remera'),(51,'zapatillarda','','zapatilla black','hombre','foto-1748358193849-894368710.jpg','68000','zapatilla');
+/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-28 21:10:45
---
+++ Registros para BD de Prueba/Proyecto-ingenieria-web-2_usuarios.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: Proyecto-ingenieria-web-2
+-- ------------------------------------------------------
+-- Server version	8.0.42-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (6,'raul','perez','pablo@gmail.com','3888655555','usuario','$2b$10$1pXq1Cm8g416q94evIDyI.jVYDfT5h/iCFOYOPZldcA1pwba0vyx.'),(7,'raul','perez','joaquin@gmail.com','3888655555','usuario','$2b$10$NXAOZsC1MzJTxedL8FKmr.CLiBZ/lGaz65QPI6zkFrr0Tb5yhgVpq'),(8,'bruno','garubaldu','bruno@gmail.com','3888655555','usuario','$2b$10$yBt2oxisAz4IFgo1tRf9IOxVztgmKP/1PjMgKy8FnGfXyvlPmBL9C'),(13,'pjiojoijoij','koijoijiojiojj','joaqun@gmail.com',NULL,'usuario','$2b$10$RVkckeo9r96UzP4tYFmnb.Q1Tjr.O3UZfVyr0Ar6aBdyoKLbvYTOG'),(14,'okoko','koijoijiojiojj','jun@gmail.com',NULL,'usuario','$2b$10$mC9Zva4znrEwrvrFU80bDeRMGPiquIGI/A2GRYOtIw337Capj7Fem'),(15,'raul ','cernik','raulcernik@gmail.com',NULL,'usuario','$2b$10$4ple1qvOAQe0vyWeOHDFneVImwE/PqUagsD9rKbinm0oGXPGueoGW'),(16,'raul ','cernik','rlcernik@gmail.com',NULL,'usuario','$2b$10$FvVIZaJO8gsQavayBSLTG.lpf6vusWVmqb2.d7vCmhGx0zSZt2wxi'),(17,'raul ','cernik','rlcerik@gmail.com',NULL,'usuario','$2b$10$VhC4imjRB5lX52yxLsKLpOSkHor6n6NsCYd/sKoPjKq8qN7DeOTo.'),(18,'raul kpokp','cernik','rlrik@gmail.com',NULL,'usuario','$2b$10$TZ8gnqtegui84sR4KsxjJ.vrXYW96YozySS5CY0GPaxa4YHn9jl9a'),(19,'raul kpokp','cernik','rlnnnnnnrik@gmail.com',NULL,'usuario','$2b$10$kveG7ApgQyqEs1psG0wTkOXAbEvP154vAuW7ix3AQ9QjXQRca3kFe'),(20,'raul kpokp','cernik','rlnnnnniiinrik@gmail.com',NULL,'usuario','$2b$10$VwN3RZqVCb0CXDkB3GP1bebml4sjgaZFhaft95jU0elO/2Yx7.MWO'),(21,'raul kpokp','cernik','rloiinrik@gmail.com',NULL,'usuario','$2b$10$f8aZWM2nSIoBWjj/E7kkmew6L/MDAcXjELp189ls7/Ddl.2YgAs0u'),(22,'raulpatopro','cernik','rloiiknrik@gmail.com',NULL,'usuario','$2b$10$SMMyIYMf3uLObTjksXFTqOyRrUl2fc4N1oog8sIA8tVzVlGj9Lof2'),(23,'raulpatopro','cernik','popopop@gmail.com',NULL,'usuario','$2b$10$LTdFci6gARsvqNRzoaZYe.GoOfWuIiHmVogMYCY3NYHx8J8i.MAkW'),(24,'raulpatopro','cernik','ppop@gmail.com',NULL,'usuario','$2b$10$cbiSQh103FgFvGKDfdtld.uiujsC9SCVYFxWdkCoDvpKxkH/x2TYm'),(25,'raulpatopro','cernik','ppoiiiiip@gmail.com',NULL,'usuario','$2b$10$u3O2zmGDbCYGXqZsMsS5HOQW.A.yv84aaGB.vWfI0N3NDvr5ILQJi'),(27,'admin','admin','admin@gmail.com',NULL,'admin','$2b$10$svgqaFq0jXsthzfaQGSx9etQQgQartOzuQQfEDXsKX9xYAiqq/Ex2');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-28 21:10:45
